9fc604f1dec1fe4d7769ad860cda47e2bfe6002a
mrz1880 <mrz1880@gmail.com>
2022-08-08 23:11:27 +0200

    feat(event): implement Trigger Event use case
    the index.js is used to expose the Trigger Event to the user of the sdk

---
+++ useCases/TriggerEvent.js
@@ -0,0 +1,55 @@
+const { ReadConfigFile } = require('./ReadConfigFile')
+const { Event } = require('../domain/event')
+const { locateService } = require('../infrastructure/config/locateService')
+const { RecordEvent } = require('./RecordEvent')
+const { SendEvent } = require('./SendEvent')
+const { CreateIdentity } = require('./CreateIdentity')
+const { CreateAlias } = require('./CreateAlias')
+const { GetAliasById } = require('./GetAliasById')
+
+async function TriggerEvent({ data }) {
+  const service = locateService()
+
+  const event = new Event(data)
+
+  if (config.mode === 'live') {
+    if (event.createsAlias === 'yes') {
+      const {
+        data: {
+          ids: [identityId],
+        },
+      } = await CreateIdentity({
+        companyId: data.companyId,
+        eventsApiHttpClient: service.eventsApiHttpClient,
+      })
+      const {
+        data: { id },
+      } = await CreateAlias({
+        data: { identityId, status: 'assigned' },
+        phantomApiHttpClient: service.phantomApiHttpClient,
+      })
+      event.aliasId = id
+    } else {
+      const { data: alias } = await GetAliasById({
+        aliasId: event.aliasId,
+        phantomApiHttpClient: service.phantomApiHttpClient,
+      })
+    }
+    const recordedEvent = await RecordEvent({ event, phantomApiHttpClient: service.phantomApiHttpClient })
+    return SendEvent({ eventId: recordedEvent.id, phantomApiHttpClient: service.phantomApiHttpClient })
+  }
+  if (event.createsAlias === 'yes') {
+    const {
+      data: { id },
+    } = await CreateAlias({ data: { status: 'assigned' }, phantomApiHttpClient: service.phantomApiHttpClient })
+    event.aliasId = id
+  } else {
+    const { data: alias } = await GetAliasById({
+      aliasId: event.aliasId,
+      phantomApiHttpClient: service.phantomApiHttpClient,
+    })
+  }
+  return RecordEvent({ event, phantomApiHttpClient: service.phantomApiHttpClient })
+}
+
+module.exports = { TriggerEvent }
