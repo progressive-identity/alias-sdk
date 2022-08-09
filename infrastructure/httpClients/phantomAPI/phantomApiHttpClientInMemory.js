14233431b6f9ecfa8e912ac5c2a569b3b23894e7
mrz1880 <mrz1880@gmail.com>
2022-08-09 14:59:18 +0200

    refactor(get-alias-by-id): extract arrange part to be done in the test

--- infrastructure/httpClients/phantomAPI/phantomApiHttpClientInMemory.js
+++ infrastructure/httpClients/phantomAPI/phantomApiHttpClientInMemory.js
@@ -77,32 +77,7 @@ class PhantomApiHttpClientInMemory {
     },
   }
 
-  getAliasesAliasId200Response = {
-    code: 200,
-    data: {
-      id: 10,
-      identityId: 32,
-      status: 'assigned',
-      appIdentifiers: [
-        {
-          id: 1,
-          value: 'myValue',
-          type: {
-            id: 2,
-            name: 'mailchimp-member-id',
-            description: 'id of a member in mailchimp',
-          },
-        },
-      ],
-      selectors: [
-        {
-          id: 1,
-          value: 'myValue',
-          dataTypeInstanceId: 'string',
-        },
-      ],
-    },
-  }
+  getAliasesAliasId200Response
 
   async post({ endpoint }) {
     const responseByEndpoint = {
@@ -120,6 +95,10 @@ class PhantomApiHttpClientInMemory {
   async getById({ endpoint }) {
     const responseByEndpoint = { [endpoints.aliases]: this.getAliasesAliasId200Response }
     return responseByEndpoint[endpoint]
+  }
+
+  feedGetAliasByIdResponse(response) {
+    this.getAliasesAliasId200Response = response
   }
 }
 
--- test/useCases/GetAliasById.test.js
+++ test/useCases/GetAliasById.test.js
@@ -7,7 +7,35 @@ describe('GetAliasById', () => {
       id: 99,
     }
     const phantomApiHttpClient = new PhantomApiHttpClientInMemory()
+    phantomApiHttpClient.feedGetAliasByIdResponse({
+      code: 200,
+      data: {
+        id: 10,
+        identityId: 32,
+        status: 'assigned',
+        appIdentifiers: [
+          {
+            id: 1,
+            value: 'myValue',
+            type: {
+              id: 2,
+              name: 'mailchimp-member-id',
+              description: 'id of a member in mailchimp',
+            },
+          },
+        ],
+        selectors: [
+          {
+            id: 1,
+            value: 'myValue',
+            dataTypeInstanceId: 'string',
+          },
+        ],
+      },
+    })
+
     const response = await GetAliasById({ params, phantomApiHttpClient })
+
     expect(response).toEqual(phantomApiHttpClient.getAliasesAliasId200Response)
   })
 })
