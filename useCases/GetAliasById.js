08d801ff8d9940800bd7e202ff90b01b0f4deb7d
mrz1880 <mrz1880@gmail.com>
2022-08-19 16:28:26 +0200

    feat(alias): implement GetAliasByAppIdentifierAndAppIdentifierType use case
    remove getAliasById usecase

--- useCases/GetAliasById.js
+++
@@ -1,7 +0,0 @@
-const { endpoints } = require('../infrastructure/httpClients/phantomAPI/endpoints')
-
-async function GetAliasById({ aliasId, phantomApiHttpClient }) {
-  return phantomApiHttpClient.getById({ endpoint: endpoints.aliases, id: aliasId })
-}
-
-module.exports = { GetAliasById }
