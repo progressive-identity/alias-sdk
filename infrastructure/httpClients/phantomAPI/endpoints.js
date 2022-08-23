27819bd3fb078e4dd968479fb1abdfb2831db50e
mrz1880 <mrz1880@gmail.com>
2022-08-23 16:03:46 +0200

    refactor(project): delete former files

--- infrastructure/httpClients/phantomAPI/endpoints.js
+++
@@ -1,3 +0,0 @@
-const endpoints = Object.freeze({ events: 'events', sendEvents: 'send-events', aliases: 'aliases', alias: 'alias' })
-
-module.exports = { endpoints }
--- infrastructure/httpClients/phantomAPI/phantomApiHttpClientAxios.js
+++
@@ -1,3 +0,0 @@
-class PhantomApiHttpClientAxios {}
-
-module.exports = { PhantomApiHttpClientAxios }
